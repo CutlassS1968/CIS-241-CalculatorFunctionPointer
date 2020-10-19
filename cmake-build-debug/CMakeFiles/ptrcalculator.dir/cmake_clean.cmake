53404a82e6a02742f90a66b399858532825fd672
evanjohns <evanlloydjohns@gmail.com>
2020-10-18 22:36:56 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/ptrcalculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ptrcalculator.dir/main.c.o"
+  "ptrcalculator"
+  "ptrcalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ptrcalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,94 @@
+//
+//  CIS 241 02, Professor Vijay Bhuse
+//  Calculator Using Function Pointers
+//
+//  Created by Evan Johns on 10/18/2020
+//
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int getOperation(char *);
+void convertToLowercase(char *);
+void multiply(double, double);
+void divide(double, double);
+void add(double, double);
+void subtract(double, double);
+void print(char *, double, double, double);
+
+const char *operationStrPtr[4] = {"multiply", "divide", "add", "subtract"};
+
+int main() {
+
+    void (*functions[4])(double, double) = {multiply, divide, add, subtract};
+
+    char uString[9];    // User's input string
+    int uOperation;     // User's selected Operation
+    double uN1, uN2;    // User's number input
+
+    do {    // Get user's input
+        printf("What calculation would you like to perform?\n"
+               "Options: Multiply, Divide, Add, Subtract\n");
+        scanf("%s", uString);
+        uOperation = getOperation(uString);
+    } while(uOperation == 4);           // Error checking user's input
+
+    printf("What numbers would you like to %s?\n", operationStrPtr[uOperation]);
+
+    do {    // Get user's first number
+        printf("Number 1: ");
+    } while(scanf("%lf", &uN1) != 1);   // Error checking user's input
+
+    do {    // Get user's second number
+        printf("Number 2: ");
+    } while(scanf("%lf", &uN2) != 1);   // Error checking user's input
+
+    (*functions[uOperation])(uN1, uN2); // Calculate and print the results
+
+    return 0;
+}
+
+// Return the corresponding operation number
+int getOperation(char *sPtr) {
+    convertToLowercase(sPtr);
+    for (int i = 0; i < 4; ++i) {
+        if (strcmp(sPtr, operationStrPtr[i]) == 0) {
+            return i;
+            }
+        }
+    return 4;
+}
+
+// Converts a string input to lowercase
+void convertToLowercase(char *sPtr) {
+    while (*sPtr != '\0') {
+        *sPtr = tolower(*sPtr);
+        ++sPtr;
+    }
+}
+
+// Multiplies the two numbers and calls print
+void multiply(double n1, double n2) {
+    print("*", n1, n2, n1 * n2);
+}
+
+// Divides the two numbers and calls print
+void divide(double n1, double n2) {
+    print("/",n1, n2, n1 / n2);
+}
+
+// Adds the two numbers and calls print
+void add(double n1, double n2) {
+    print("+", n1, n2, n1 + n2);
+}
+
+// Subtracts the two numbers and calls print
+void subtract(double n1, double n2) {
+    print("-", n1, n2, n1 - n2);
+}
+
+// Prints the result of a calculation
+void print(char calc[], double n1, double n2, double r) {
+    printf("%.2f %s %.2f = %.2f", n1, calc, n2, r);
+}
